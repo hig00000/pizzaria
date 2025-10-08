dce23eab34ab1044279f250dec6ff4008d977231
higor santos <117781999+hig00000@users.noreply.github.com>
2025-10-08 17:28:22 -0300

    Como não estou programando no meu computador de costume precisei  baixar o projeto e melhorar ele por outra IDE fiz a alteração para deixar responsivo e tentei melhorar a interface

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navLinks = document.querySelector('.nav-links');
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
 
-    menuToggle.addEventListener('click', () => {
-        // Adiciona ou remove a classe 'active' da lista de links
-        navLinks.classList.toggle('active');
-    });
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    menuToggle.classList.toggle("active");
+  });
 });
